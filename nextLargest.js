bc8e31ff002597a29d9a4273b59999ac1ca77004
LavanyaBS2022 <lavanyabs2022@gmail.com>
2022-11-28 11:36:14 +0530

    second commit

--- nextLargest.js
+++ nextLargest.js
@@ -1,9 +1,32 @@
-var arr = [3, 6, 2, 56, 32, 5, 89, 32];
-var largest = arr[0];
-
-for (var i = 0; i < arr.length; i++) {
-    if (largest < arr[i] ) {
-        largest = arr[i];
+function nextLargest(array,value)
+{
+    let x=0;
+    let dist=[];
+    for(var i=0;i<array.length;i++)
+    {
+        if(value==array[i])
+        {
+            x=i;
+        }
     }
+
+    for(var i=0;i<array.length;i++)
+    {
+       if(array[i]>value)
+       {
+         var temp=Math.abs(x-i);
+         dist.push(temp);
+       }
+    }
+     let result;
+    let dummy;
+    for(var i=0;i<dist.length-1;i++)
+    {
+      dummy=Math.min(...dist);
+    }
+    result=array[dummy+x];
+    console.log(result);
 }
-console.log(largest);
+
+var arr=[1,4,3,2,5,7];
+nextLargest(arr,1);
